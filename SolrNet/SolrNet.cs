51c0cffe442e95fe5b08fa05799bd46004fc9505
Aaron Rosenberger <arosenberger@silverchair.com>
2023-02-10 14:44:12 -0500

    upgrade CommonServiceLocator to 2.0.6

--- SolrNet/SolrNet.cs
+++ SolrNet/SolrNet.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using Microsoft.Practices.ServiceLocation;
+using CommonServiceLocator;
 using SolrNet.Impl;
 using SolrNet.Impl.DocumentPropertyVisitors;
 using SolrNet.Impl.FacetQuerySerializers;
